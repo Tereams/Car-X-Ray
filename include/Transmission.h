f96a85f2833935efa70621b09c5a61ad4c7bb567
tereams <tianyue.chen@outlook.com>
2026-05-16 12:28:39 -0700

    Create Transmission.h

---
+++ include/Transmission.h
@@ -0,0 +1,39 @@
+#pragma once
+
+struct RotationalState {
+    double torqueNm;
+    double rpm;
+};
+
+class Transmission {
+public:
+    Transmission();
+
+    // control
+    void setGear(int gear);
+    void shiftUp();
+    void shiftDown();
+
+    // physical input
+    void setInput(const RotationalState& input);
+
+    // simulation
+    void update(double deltaTime);
+
+    // physical output
+    RotationalState getOutput() const;
+
+    // state query
+    int getCurrentGear() const;
+    double getCurrentGearRatio() const;
+    bool isInNeutral() const;
+    bool isInReverse() const;
+
+private:
+    int currentGear_;
+
+    RotationalState input_;
+    RotationalState output_;
+
+    double efficiency_;
+};
